d37c78b7bfe93686b2fa021df2f5c3bcbe669138
422985573 <422985573@qq.com>
2026-04-11 12:49:13 +0800

    222

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,4 +3,4 @@ def test_config_loads():
     import config
     assert config.SECRET_KEY
     assert config.PER_PAGE >= 1
-    assert config.POSTCODE_API_BASE_URL.startswith('http')
+    assert config.POSTCODE_CSV_PATH
